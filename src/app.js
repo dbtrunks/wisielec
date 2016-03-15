c123981af788dee552ddc23309ad0785d4f3b43a
dbtrunks <PIO.CUD@gmail.com>
2016-03-15 01:19:55 +0100

    commit1

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 export class App {
-  configureRouter(config, router) {
-    config.title = 'Aurelia';
-    config.map([
-      { route: ['', 'game'], name: 'game',      moduleId: 'game',      nav: true, title: 'Graj' },
-      { route: 'top',         name: 'top',        moduleId: 'top',        nav: true, title: 'Ranking' },
-      { route: 'info',  name: 'info', moduleId: 'info', nav: true, title: 'Info' }
-    ]);
+    configureRouter(config, router) {
+        config.title = 'Aurelia';
+        config.map([
+            { route: ['', 'game'], name: 'game', moduleId: 'game', nav: true, title: 'Graj' },
+            { route: 'top', name: 'top', moduleId: 'top', nav: true, title: 'Ranking' },
+            { route: 'info', name: 'info', moduleId: 'info', nav: true, title: 'Info' }
+        ]);
 
-    this.router = router;
-  }
+        this.router = router;
+    }
 }
